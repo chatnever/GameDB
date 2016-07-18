1f25e0481399dcfaed1e5aab4b0cc7f6674e1740
Dzung Nguyen <nddung92@gmail.com>
2016-07-18 21:49:37 +0700

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :games do
+  	resources :reviews
+  end
+  root 'games#index'
+
+end
---
+++ db/migrate/20160718084828_add_download_link_to_games.rb
@@ -0,0 +1,4 @@
+class AddDownloadLinkToGames < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
